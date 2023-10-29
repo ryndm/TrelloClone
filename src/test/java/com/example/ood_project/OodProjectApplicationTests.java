4fb8689f60cdc4efbe1cd89eb63ce98d20fdf42e
Ryan <dmelloryan11@gmail.com>
2023-10-29 13:36:08 -0400

    first commit

---
+++ src/test/java/com/example/ood_project/OodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ood_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
