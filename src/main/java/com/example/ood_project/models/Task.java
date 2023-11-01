5ebeb30447b08763902584f67eacb3b65f68679e
deepthijv <deepthijagalur@gmail.com>
2023-10-31 23:07:53 -0400

    Updated controller and models

--- src/main/java/com/example/ood_project/models/Task.java
+++ src/main/java/com/example/ood_project/models/Task.java
@@ -5,9 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
@@ -23,11 +21,13 @@ public class Task {
     @Column(nullable = true)
     private String description; // by default null
 
+
     @Column(nullable = true)
     private TaskState state; // by default todo
 
     @Column(nullable = true)
     private LocalDateTime created_at; // by default current time. Should not be given in input
+
 
     @ManyToMany(mappedBy = "tasks") // mappedBy points to the "tasks" field in the User class
     private Set<User> users; // by default null or an empty set
@@ -35,6 +35,14 @@ public class Task {
     @Column(nullable = true)
     private List<String> comments;
 
+    @PrePersist
+    public void prePersist() {
+        created_at = LocalDateTime.now();
+        description=null;
+        state=TaskState.TODO;
+        users=new HashSet<>();
+        comments=new ArrayList<>();
+    }
     // Validate all the fields
 }
 
